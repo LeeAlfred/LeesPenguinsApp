6890bc42fdbb669cf2468c76c92d899f834e9504
LeeAlfred <emailleesampson@gmail.com>
2025-05-21 16:55:26 +0200

    Initial commit: Set up project with basic Compose UI, date display, and image loading.added a val currentDate = LocalDate.of to test.

--- app/src/main/java/com/example/lees_penguins/ui/theme/Theme.kt
+++ app/src/main/java/com/example/lees_penguins/ui/theme/Theme.kt
@@ -1,6 +1,4 @@
 package com.example.lees_penguins.ui.theme
-
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
